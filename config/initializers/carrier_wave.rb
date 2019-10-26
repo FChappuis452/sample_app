2929748ea77b1b5385c6f1340ea9abba3266dfb2
Frederic Chappuis <fchappuis452@gmail.com>
2019-10-26 19:26:44 +0000

    bug fix cloudinary

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     Cloudinary.config do |config|
       # Configuration for cloudinary
-      config.cloud = ENV['C_BUCKET']
+      config.cloud_name = ENV['C_BUCKET']
       config.api_key = ENV['C_API_KEY']
       config.api_secret = ENV['C_API_SECRET']
       config.secure = ENV['C_SECURE']
